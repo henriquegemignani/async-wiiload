5a2101bec5fbef81ddbf6274c6db521587cb636d
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2020-11-07 17:01:50 +0200

    Drop Python 3.6

--- setup.py
+++ setup.py
@@ -36,12 +36,11 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     setup_requires=[
         "setuptools_scm",
     ],
