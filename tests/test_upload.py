585c0b815708e72ed126d2e1c3a8cef51514d97b
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2023-07-25 17:37:46 +0300

    More cleanup

--- tests/test_upload.py
+++ tests/test_upload.py
@@ -18,8 +18,7 @@ async def test_upload_bytes(unused_tcp_port, mocker):
     )
 
     expected_data = (
-        b"HAXX\x00\x05\x00\x08\x00\x00\x00\x0c\x00\x00\x00\x04x\x9c3426\x01\x00"
-        b"\x01\xf8\x00\xcbme\x00that\x00"
+        b"HAXX\x00\x05\x00\x08\x00\x00\x00\x0c\x00\x00\x00\x04x\x9c3426\x01\x00\x01\xf8\x00\xcbme\x00that\x00"
     )
 
     await wiiload.upload_bytes(b"1234", ["me", "that"], "localhost", unused_tcp_port)
@@ -32,9 +31,7 @@ async def test_upload_bytes(unused_tcp_port, mocker):
 
 
 async def test_upload_file(tmp_path, mocker):
-    mock_upload_bytes = mocker.patch(
-        "wiiload.upload.upload_bytes", new_callable=AsyncMock
-    )
+    mock_upload_bytes = mocker.patch("wiiload.upload.upload_bytes", new_callable=AsyncMock)
 
     path = tmp_path.joinpath("somewhere.bin")
     path.write_bytes(b"foobar")
@@ -43,6 +40,4 @@ async def test_upload_file(tmp_path, mocker):
     await wiiload.upload_file(path, ["foo"], "localhost")
 
     # Assert
-    mock_upload_bytes.assert_awaited_once_with(
-        b"foobar", ["somewhere.bin", "foo"], "localhost", 4299
-    )
+    mock_upload_bytes.assert_awaited_once_with(b"foobar", ["somewhere.bin", "foo"], "localhost", 4299)
